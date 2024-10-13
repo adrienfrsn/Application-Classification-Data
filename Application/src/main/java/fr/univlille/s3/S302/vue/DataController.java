5566ade29d98b0334d42b6db2d221fbc405807b7
benjamin roget <benjamin.roget.etu@univ-lille.fr>
2024-10-13 17:36:47 +0200

    color & category

--- Application/src/main/java/fr/univlille/s3/S302/vue/DataController.java
+++ Application/src/main/java/fr/univlille/s3/S302/vue/DataController.java
@@ -2,14 +2,14 @@ package fr.univlille.s3.S302.vue;
 
 import fr.univlille.s3.S302.model.*;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Tooltip;
 import javafx.util.Pair;
 
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class DataController {
 
@@ -17,12 +17,17 @@ public class DataController {
     @FXML
     ScatterChart<String , Number> chart;
 
-    Map<Coordonnee, Iris> data;
+    List<Pair<XYChart.Data<String, Number>, Data>> data ;
+
+    private Map<String, String> categorieColor = new HashMap<>();
 
     DataManager<Data> dataManager = new DataManager<>();
 
     @FXML
     public void initialize() {
+        data = new ArrayList<>();
+        categorieColor.put("Unknown", "black");
+
         chart.setTitle("Scatter Chart");
         dataManager.loadData("iris.csv");
 
@@ -36,24 +41,58 @@ public class DataController {
 
         for (Data f : dataManager.getDataList()) {
             Pair<String, Number> choosenAttributes = f.getChoosenAttributes();
-            //Coordonnee c = new Coordonnee(choosenAttributes.getKey(), choosenAttributes.getValue());
-            System.out.println(choosenAttributes.getKey() + " " + choosenAttributes.getValue());
-            series.getData().add(new XYChart.Data<>(choosenAttributes.getKey(), choosenAttributes.getValue()));
-
-
+            Coordonnee c = new Coordonnee(Double.parseDouble(choosenAttributes.getKey()), choosenAttributes.getValue().doubleValue());
+            XYChart.Data<String , Number> node = new XYChart.Data<>(choosenAttributes.getKey(), choosenAttributes.getValue());
+            data.add(new Pair<>(node, f));
+            series.getData().add(node);
         }
         chart.getData().add(series);
 
         for (final XYChart.Series<String, Number> s : chart.getData()) {
             for (final XYChart.Data<String, Number> data : s.getData()) {
+                Data d = getNode(data);
                 Tooltip tooltip = new Tooltip();
-                tooltip.setText(data.getXValue() + " : " + data.getYValue());
+                tooltip.setText(d.getCategory() + "\n" + data.getXValue() + " : " + data.getYValue());
                 tooltip.setShowDuration(javafx.util.Duration.seconds(10));
                 tooltip.setShowDelay(javafx.util.Duration.seconds(0));
                 Tooltip.install(data.getNode(), tooltip);
+                data.getNode().setOnMouseEntered(event -> {
+                    data.getNode().setScaleX(1.5);
+                    data.getNode().setScaleY(1.5);
+                });
+                data.getNode().setOnMouseExited(event -> {
+                    data.getNode().setScaleX(1);
+                    data.getNode().setScaleY(1);
+                });
+
+                setNodeColor(data.getNode(), d.getCategory());
+
             }
         }
 
+
+
+    }
+
+    private Data getNode(XYChart.Data<String, Number> data) {
+        for (Pair<XYChart.Data<String, Number>, Data> d : this.data) {
+            if (d.getKey() == data) {
+                return d.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void setNodeColor(Node node, String category) {
+        if (node == null) {
+            return;
+        }
+        if (!categorieColor.containsKey(category)) {
+            String color = "rgb(" + (int) (Math.random() * 256) + "," + (int) (Math.random() * 256) + "," + (int) (Math.random() * 256) + ")";
+            categorieColor.put(category, color);
+        }
+
+        node.setStyle("-fx-background-color: " + categorieColor.get(category) + ";");
     }
 
 }
